b5e4d992bab73583af004cb14cd93a1fb826e0de
davidebbo <david.ebbo@microsoft.com>
2013-01-31 16:10:52 -0800

    Info before Error

--- MvcApplication4App/Controllers/HomeController.cs
+++ MvcApplication4App/Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace MvcApplication4App.Controllers
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
+            System.Diagnostics.Trace.TraceInformation("Info statement");
             System.Diagnostics.Trace.TraceError("Error statement");
             System.Diagnostics.Trace.TraceWarning("Warning statement");
-            System.Diagnostics.Trace.TraceInformation("Info statement");
 
             return View();
         }
